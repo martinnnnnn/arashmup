148a157c37e07c21b2eee8ea01d146ea5be203c0
martinnnnnn <martin.penard@gmail.com>
2020-12-04 15:41:18 +0100

    spawning - cleanup and simplification

--- Assets/_assets/1.scripts/Weapons/WeaponPickup.cs
+++ Assets/_assets/1.scripts/Weapons/WeaponPickup.cs
@@ -8,18 +8,17 @@ using Photon.Pun;
 using UnityEngine.SceneManagement;
 using System.IO;
 
-public class WeaponPickup : MonoBehaviour
+public class WeaponPickup : SpawnObject
 {
     public Weapon.Type weaponType;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("mouahahya");
         WeaponController weaponController = other.GetComponent<WeaponController>();
         if (weaponController != null)
         {
             weaponController.Equip(weaponType);
-            GetComponent<SpawnObject>().DestroySelf();
+            DestroySelf();
         }
     }
 }
