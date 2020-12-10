a002b4063bbf61758fc0e84417bacc61da3c4b30
martinnnnnn <martin.penard@gmail.com>
2020-12-10 01:26:13 +0100

    adding events from great talk from Ryan Hipple

---
+++ Assets/_assets/1.scripts/Events/Editor/EventEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Arashmup
+{
+    [CustomEditor(typeof(GameEvent), editorForChildClasses: true)]
+    public class EventEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent e = target as GameEvent;
+            if (GUILayout.Button("Raise"))
+                e.Raise();
+        }
+    }
+}
